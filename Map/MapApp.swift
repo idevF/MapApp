f98dbc6a65a5f43c2c679f87a52fc28e87f2feab
idevF <124703341+idevF@users.noreply.github.com>
2023-03-20 23:28:42 +0300

    Initial Commit

---
+++ Map/MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapApp.swift
+//  Map
+//
+//  Created by idevF on 20.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
